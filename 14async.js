426463fed34605be92f41e3bba58ea294ab872ca
Prajjaval08 <prajjavalp@gmail.com>
2026-06-19 21:19:39 +0530

    async

---
+++ 14async.js
@@ -0,0 +1,36 @@
+const {readFile} = require('fs')
+const { result } = require('lodash')
+const { resolve } = require('path')
+
+const  getText = (path) => {
+    return new Promise((resolve, reject) => {
+readFile('./content/first.txt' , 'utf8' , (err , data) => {
+
+    if(err){
+        reject(err)
+    }
+
+    else{
+       resolve(data)
+    }
+})
+    })
+}
+
+getText('./content/first.txt')
+.then(result => console.log(result))
+.catch((err) => console.log(err))
+
+
+const start = async() => {
+   try {
+    const first =await getText('./content/first.txt')
+   console.log(first)
+}  catch(error){
+    console.log(error)
+}
+}
+
+
+
+start()
